ffd096404cc9d978c8c54a179b172f64e0d18491
maria-nusrat-2005 <marianusrat.tonu34@gmail.com>
2025-12-04 23:59:24 +0600

    btn alert

--- script/index.js
+++ script/index.js
@@ -8,17 +8,20 @@ const loadLevelWord = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayalevelWord(data.data));
+    .then((data) => displaylevelWord(data.data));
 };
 
-const displayalevelWord = (words) => {
+const displaylevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
-
+  if (words.length === 0) {
+    alert("No words found for this lesson.");
+    return;
+  }
   words.forEach((word) => {
     console.log(word);
     const card = document.createElement("div");
-    card.innerHTML = ` <div class="bg-white rounded-xl  w-11/12  shadow-lg text-center p-5 mx-auto">
+    card.innerHTML = ` <div class="bg-white rounded-xl  shadow-lg text-center p-5 ">
       <h2 class="font-bold text-xl">"${word.word}"</h2>
       <p>Meaning /Pronounciation</p>
       <div>"${word.meaning} / ${word.pronunciation}"</div>
@@ -30,7 +33,6 @@ const displayalevelWord = (words) => {
     wordContainer.append(card);
   });
 };
-loadLevelWord();
 
 const displaylessons = (lessons) => {
   //get the container empty
@@ -43,7 +45,7 @@ const displaylessons = (lessons) => {
     console.log(lesson);
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-                <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary "
+                <button onclick="loadLevelWord(${lesson.level_no}) " class="btn btn-outline btn-primary "
                 ><i class="fa-solid fa-book-open"></i> lesson - ${lesson.level_no}</button>    
              
     `;
